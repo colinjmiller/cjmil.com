6a5b53bdcd39b68967db4d180c1f05f728013527
Colin Miller <cjmiller@uw.edu>
2015-01-22 00:52:28 -0800

    Adding multilanguage support

--- portfolio.php
+++ portfolio.php
@@ -1,6 +1,7 @@
 <?php
 
   require_once('view-logic/views.php');
+  require_once('en.php');
 
 ?>
 <?= View::header_logic("Honors Portfolio") ?>
@@ -22,32 +23,30 @@
           </li>
         </ul>
       </nav>
-      <h2>A Constant State of Learning</h2>
+      <h2><?= t('portfolio_statement_title') ?></h2>
       <p>
         <code>
           public final boolean LEARNING = true;
         </code>
       </p>
       <p>
-        The last sixteen years of my life have involved learning. From ages five until thirteen, I learned the basics of life at home under the guidance of my mother. After eighth grade, I went to a public high school and continued my studies in a broad spectrum. Entering college, I believed my trend of learning was coming to a close.
+        <?= t('portfolio_p1') ?>
       </p>
       <p>
-        This is not the case; I am still learning. I will always be learning.
+        <?= t('portfolio_p2') ?>
       </p>
       <img id="learning-img" src="portfolio/areas.svg" alt="Areas of learning as I get older" />
       <p>
-        Software Development is an intimidating field to me. All of my co-workers have more experience than me and seem to effortlessly write beautiful code while still finding time to help teach me. Each day, I learn at least five new things.
+        <?= t('portfolio_p3') ?>
       </p>
       <p>
-        Reflecting on that, I'd rather learn five new things each day and feel a little overwhelmed, rather than not learn anything at all. That's where <em>A Constant State of Learning</em> comes into play. A reddit user once asked the question,
-        <q>When does the change from, 'This is complicated, I better figure it out!' to 'This is complicated, YOU figure it out!' happen?</q>
-        My goal in life is to never make that switch.
+        <?= t('portfolio_p4') ?>
       </p>
       <p>
-        This portfolio is a culmination of my experiences at the University of Washington. Through it, I wish to reflect on my past so that I can better my future. I began this portfolio in late 2011 and, for the first few quarters, wrote descriptions of every class I took. However, the things I wrote have caused my present self to cringe, so I have opted to disclude many of those samples and edit those that remain. For posterity, <a href="http://cjmil.com/old/">here's a link to the older, more cringey me</a>, complete with a web design from 2012.
+        <?= t('portfolio_p5') ?>
       </p>
       <p>
-        If I'm being realistic with myself, I will look back on the things I wrote here and cringe again when I'm more mature.
+        <?= t('portfolio_p6') ?>
       </p>
     </div>
 <?= View::footer_logic() ?>
