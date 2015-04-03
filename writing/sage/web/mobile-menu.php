3fc1db92abce5d70d7213350ea1f12692f63dc67
Colin Miller <cjmiller@uw.edu>
2015-04-02 18:48:37 -0700

    Using escape html instead of codepen for mobile menu article

--- writing/sage/web/mobile-menu.php
+++ writing/sage/web/mobile-menu.php
@@ -7,18 +7,18 @@
   <?= View::navigation_bar("Home") ?>
     <article class="mobile-menu container">
       <img class="main-img u-light-margin" src="/images/sage/sage_on_mobile.svg" alt="Sage Waving" />
-      <div class="content-column">
+      <section class="content-column">
         <header>
           <h1>Expandable Mobile Menu in CSS</h1>
           <h2>...And it's accessible, too!</h2>
         </header>
-        <section class="publication-times">
+        <div class="publication-times">
           <p>Published: <time datetime="2015-03-27">March 27th, 2015</time>.</p>
-        </section>
-        <section class="introduction">
+        </div>
+        <div class="introduction">
           <p>Nowadays, web developers use <em>Mobile-First Responsive Web Design</em> to give mobile phone users a great experience on their website. If you go to this website on a phone or resize your browser to a smaller size, you'll see that it looks like a familiar phone interface. In particular, the menu options are hidden by default but become available after clicking the mobile menu button. That button's functionality depends entirely on CSS; there's no Javascript involved! Interested how it works? Great, let's go!</p>
-        </section>
-      </div>
+        </div>
+      </section>
       <section class="mobile-menu-example">
         <h3>Exhibit A: The Mobile Menu</h3>
         <figure>
@@ -27,9 +27,28 @@
         </figure>
       </section>
       <section class="mobile-menu-html">
+        <h3>The Code</h3>
         <p>The secret to achieving this effect involves using a checkbox and label. Let's take a look at the HTML to see how it's all laid out:</p>
-        <p data-height="268" data-theme-id="0" data-slug-hash="XJQxGQ" data-default-tab="result" data-user="cjmiller" class='codepen'>See the Pen <a href='http://codepen.io/cjmiller/pen/XJQxGQ/'>Mobile Menu in CSS</a> by Colin Miller (<a href='http://codepen.io/cjmiller'>@cjmiller</a>) on <a href='http://codepen.io'>CodePen</a>.</p>
-<script async src="//assets.codepen.io/assets/embed/ei.js"></script>
+<pre>
+<code>&lt;!-- The mobile navbar magic --&gt;
+
+&lt;input type=&quot;checkbox&quot; id=&quot;mobile-nav&quot; /&gt;
+&lt;label for=&quot;mobile-nav&quot;&gt;
+  &lt;img src=&quot;/images/mobile_menu.svg&quot; alt=&quot;Mobile Menu&quot; /&gt;
+&lt;/label&gt;
+
+&lt;!-- The actual navigation bar --&gt;
+
+&lt;div class=&quot;row&quot;&gt;
+  &lt;a class=&quot;navbar-item three columns&quot; href=&quot;/&quot;&gt;
+    &lt;span&gt;Home&lt;/span&gt;
+  &lt;/a&gt;
+  &lt;a class=&quot;navbar-item three columns&quot; href=&quot;/projects/&quot;&gt;
+    &lt;span&gt;Another Item...&lt;/span&gt;
+  &lt;/a&gt;
+&lt;/div&gt;
+</code>
+</pre>
       </section>
     </article>
 <?= View::footer_logic() ?>
