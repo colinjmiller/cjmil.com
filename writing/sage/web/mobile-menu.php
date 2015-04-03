296534e9a321cbc4aa4c8792bb23f070da228f3c
Colin Miller <cjmiller@uw.edu>
2015-04-02 21:17:07 -0700

    Finishing intial draft of mobile menu writing

--- writing/sage/web/mobile-menu.php
+++ writing/sage/web/mobile-menu.php
@@ -4,7 +4,7 @@
 
 ?>
 <?= View::header_logic("Expandable Mobile Menu in CSS | Sage Advice") ?>
-  <?= View::navigation_bar("Home") ?>
+  <?= View::navigation_bar("Writing") ?>
     <article class="mobile-menu container">
       <img class="main-img u-light-margin" src="/images/sage/sage_on_mobile.svg" alt="Sage Waving" />
       <section class="content-column">
@@ -13,7 +13,7 @@
           <h2>...And it's accessible, too!</h2>
         </header>
         <div class="publication-times">
-          <p>Published: <time datetime="2015-03-27">March 27th, 2015</time>.</p>
+          <p>Published: <time datetime="2015-04-02">April 2nd, 2015</time>.</p>
         </div>
         <div class="introduction">
           <p>Nowadays, web developers use <em>Mobile-First Responsive Web Design</em> to give mobile phone users a great experience on their website. If you go to this website on a phone or resize your browser to a smaller size, you'll see that it looks like a familiar phone interface. In particular, the menu options are hidden by default but become available after clicking the mobile menu button. That button's functionality depends entirely on CSS; there's no Javascript involved! Interested how it works? Great, let's go!</p>
@@ -27,8 +27,27 @@
         </figure>
       </section>
       <section class="mobile-menu-html">
-        <h3>The Code</h3>
+        <h3>The Mobile Navbar Code</h3>
         <p>The secret to achieving this effect involves using a checkbox and label. Let's take a look at the HTML to see how it's all laid out:</p>
+<pre>
+<code>&lt;input type=&quot;checkbox&quot; id=&quot;mobile-nav&quot; /&gt;
+&lt;label for=&quot;mobile-nav&quot;&gt;
+  &lt;img src=&quot;/images/mobile_menu.svg&quot; alt=&quot;Mobile Menu&quot; /&gt;
+&lt;/label&gt;
+</code>
+</pre>
+        <p>This might seem a little strange; why am I using form elements in my navigation menu? Well, that's where the magic of this CSS-only mobile menu becomes clear: I'm going to use the <code>:checked</code> and <code>:focus</code> pseudo-selectors of the checkbox to manipulate the page. In particular, if the checkbox is checked, I'll make that mean that I want to show the navigation menu. Otherwise, if it's unchecked, I don't want to show the navigation controls.</p>
+        <figure>
+          <img src="/images/sage/checkbox-in-menu.png" alt="An ugly checkbox in the mobile menu" title="Yuck!" />
+          <figcaption>Hmm... That checkbox looks terrible.</figcaption>
+        </figure>
+        <p>Having a checkbox in our navigation menu is more than unsightly; it's horrendous. However, how can the user activate it without it being visible? That's where the <code>label</code> comes into play! By giving the user a <code>label</code> to click on, which in turn activates the checkbox, we can make the ugly checkbox invisible! Notice that the checkbox has an <code>id</code> attribute attached to it and the <code>label</code>'s <code>for</code> attribute targets it. This is necessary.</p>
+        <p>Our next task is to make that checkbox invisible! Unfortunately, we can't get away with setting the checkbox to <code>display: none;</code> because that would remove it from the tab-index. Likewise, trying to manipulate the size by setting <code>width: 0; height: 0;</code> doesn't work in all browsers, either. The solution is to use <code>transform: scale(0);</code> to turn the checkbox invisible while still keeping it "visible" for accessibility reasons. To ensure support in browsers like IE9 and earlier versions of Chrome, Firefox, and Opera, use their vendor prefixes.</p>
+        <p>So, to reiterate: The user will see an image representing a mobile menu, but it's actually a label that triggers an invisible checkbox. We will then use some CSS magic to hide or show the navigation menu depending on whether the checkbox is <code>:checked</code> or not!</code>
+      </section>
+      <section>
+        <h3>Let's put it all together</h3>
+        <p>Now that we see the big picture, let's put it all together with the navigation menu:</p>
 <pre>
 <code>&lt;!-- The mobile navbar magic --&gt;
 
@@ -37,18 +56,53 @@
   &lt;img src=&quot;/images/mobile_menu.svg&quot; alt=&quot;Mobile Menu&quot; /&gt;
 &lt;/label&gt;
 
-&lt;!-- The actual navigation bar --&gt;
+&lt;!-- The navigation menu --&gt;
 
 &lt;div class=&quot;row&quot;&gt;
-  &lt;a class=&quot;navbar-item three columns&quot; href=&quot;/&quot;&gt;
-    &lt;span&gt;Home&lt;/span&gt;
-  &lt;/a&gt;
-  &lt;a class=&quot;navbar-item three columns&quot; href=&quot;/projects/&quot;&gt;
-    &lt;span&gt;Another Item...&lt;/span&gt;
-  &lt;/a&gt;
+  &lt;a class=&quot;navbar-item&quot; href=&quot;/&quot;&gt;Home&lt;/a&gt;
+  &lt;a class=&quot;navbar-item&quot; href=&quot;/projects/&quot;&gt;Projects&lt;/a&gt;
+  ...
 &lt;/div&gt;
 </code>
 </pre>
+        <p>And, of course, the SCSS that goes along with it:</p>
+<pre>
+<code>.navbar-item {
+  display: none;
+}
+
+#mobile-nav {
+
+  transform: scale(0);
+
+  &amp;:checked ~ .row .navbar-item {
+    display: block;
+  }
+}
+</code>
+</pre>
+        <p>By default, every <code>.navbar-item</code> is set to <code>display: none</code>. However, these items get shown when the checkbox is checked with the <code>&amp;:checked ~ .row .navbar-item</code> selector. This selector might seem a little verbose, so allow me to break it down. When the checkbox (denoted by id <code>#mobile-nav</code>) is checked, then its <code>:checked</code> pseudoselector will activate. Once this happens, I then target its sibling, <code>.row</code>, using the sibling selector (<code>~</code>). I then target <code>.row</code>'s child, <code>.navbar-item</code>, and set it to <code>display: block</code>, thus overwriting its previous style to not display.</p>
+        <p>But wait, it gets better!</p>
+      </section>
+      <section>
+        <h3>Let's make the mobile menu highlight when a user tabs to it!</h3>
+        <p>As mentioned above, our implementation still works for people using screen readers, because the checkbox is still within the tab index. However, since the <code>#mobile-nav</code> checkbox is effectively invisible, the interaction is pretty weird for people who use the tab key to navigate the web. Luckily, we can fix this with a little extra CSS!</p>
+<pre>
+<code>label {
+  border: 1px dotted transparent;
+}
+
+#mobile-nav {
+
+  ...
+
+  &amp;:focus + label {
+    border-color: black;
+  }
+}
+</code>
+</pre>
+        <p>What's going on here? By default, I'm applying a 1px invisible border on the label. I do this because when I make the border visible, I don't want things to jump around by a pixel. The cool stuff happens inside the <code>#mobile-nav</code> selector, though. When the user tabs to the <code>#mobile-nav</code> checkbox, it will active its <code>:focus</code> pseudoselector. I then target its direct sibling, <code>label</code>, and give its border a black color. This gives the user the illusion that they just tabbed to the mobile navigation button, even though it's just a label!<p>
       </section>
     </article>
 <?= View::footer_logic() ?>
