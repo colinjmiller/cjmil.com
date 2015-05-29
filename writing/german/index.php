0e65cf68ece1abcb1da6235f6818d6f2128370bf
Colin Miller <cmiller@whitepages.com>
2015-05-28 18:02:47 -0700

    Adding introduction to German article

--- writing/german/index.php
+++ writing/german/index.php
@@ -16,7 +16,13 @@
         </div>
         <div class="introduction">
           <p>
-            Deeply influenced by Mussolini's values of patriotism, national unity, and unquestioning obedience, Hitler used his power and influence in Germany to turn the country into a fascist state.
+            The Treaty of Versailles marked the end of World War I and forced Germany to accept responsability for the war. Over the following decade, Germany's Weimar Republic brought the economy to shambles through hyperinflation as it tried to pay the required war reparations. Embarassed and losing faith in their government's ability to lead, many Germans began looking for new sources of leadership.
+          </p>
+          <p>
+            Meanwhile, in the southern region of the continent, Benito Mussolini of Italy was a rising star in the political arena. Although he ardently supported the socialist party until 1915, the Great War caused him to reverse his political orientation. He formed the Fascist Party in 1919 and gained massive support from unemployed war veterans, using their influence to terrorize the established political parties. After only two years, the Fascist Party was invited to join the coalition government. By 1925, Mussolini deposed the democratic government and established himself as dictator.
+          </p>
+          <p>
+            Back in Germany, World War I veteran Adolph Hitler became interested in the German Workers' Party and the anti-Semitic, nationlistic, anti-Marxist ideas of its leader Anton Drexler. Impressed with Hitler's oratory skills and passion, he invited Hitler to join the party. Hitler accepted, joining in 1919. Deeply influenced by Mussolini's values of patriotism, national unity, and unquestioning obedience, Hitler used his power and influence in Germany to turn the country into a fascist state.
           </p>
           <p>
             Join me as I explore the political and cultural thought in Germany during the rise of fascism.
