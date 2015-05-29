ec1904736ba5042e188272c561832a4fac11e478
Colin Miller <cmiller@whitepages.com>
2015-05-28 21:09:13 -0700

    More to German article

--- writing/german/index.php
+++ writing/german/index.php
@@ -22,7 +22,10 @@
             Meanwhile, in the southern region of the continent, Benito Mussolini of Italy was a rising star in the political arena. Although he ardently supported the socialist party until 1915, the Great War caused him to reverse his political orientation. He formed the Fascist Party in 1919 and gained massive support from unemployed war veterans, using their influence to terrorize the established political parties. After only two years, the Fascist Party was invited to join the coalition government. By 1925, Mussolini deposed the democratic government and established himself as dictator.
           </p>
           <p>
-            Back in Germany, World War I veteran Adolph Hitler became interested in the German Workers' Party and the anti-Semitic, nationlistic, anti-Marxist ideas of its leader Anton Drexler. Impressed with Hitler's oratory skills and passion, he invited Hitler to join the party. Hitler accepted, joining in 1919. Deeply influenced by Mussolini's values of patriotism, national unity, and unquestioning obedience, Hitler used his power and influence in Germany to turn the country into a fascist state.
+            Back in Germany, World War I veteran Adolph Hitler became interested in the German Workers' Party and the anti-Semitic, nationlistic, anti-Marxist ideas of its leader, Anton Drexler. Impressed with Hitler's oratory skills and passion, he invited Hitler to join the party. Hitler accepted, joining in 1919. By 1920, Hitler had become the party's chief propogandist, using his speaking skills to entice crowds to attend his speehes. Hitler used his influence to rename the party to the <em>Nationalsozialistische Deutsche Arbeiterpartei</em>, or National Socialist German Workers' Party, in order to garner more popular support.
+          </p>
+          <p>
+            Deeply influenced by Mussolini's values of patriotism, national unity, and unquestioning obedience, Hitler used his power and influence in Germany to turn the country into a fascist state.
           </p>
           <p>
             Join me as I explore the political and cultural thought in Germany during the rise of fascism.
